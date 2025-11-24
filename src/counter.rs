1085c829def9e9bd61ac4fe45860f3328b5906af
EtienneC-K <etienneck@laposte.net>
2025-11-24 16:09:06 +0100

    yayaayyaayayayayyayayayayyaa

--- src/counter.rs
+++ src/counter.rs
@@ -23,6 +23,7 @@ impl CountTable {
         let table: Vec<u64> = vec![u64::MAX; Self::TABLE_SIZE];
         let counters: Vec<u32> = vec![0; Self::TABLE_SIZE];
         let skip_counter: u64 = 0;
+        assert_eq!(Self::HT_NB_BLOCKS, 1<<12);
         Self {
             table,
             counters,
@@ -37,8 +38,17 @@ impl CountTable {
         let mut inserted: bool = false;
         let mut i: usize = 0;
         while i<Self::MAX_RETRIES && !inserted {
+<<<<<<< HEAD
+            //for the address the minimizer hash determines the block,
+            //while kmer_hash and number of retries determines position inside the block
+            let block_indice = ((hashed_kmer as usize) + (i+i.pow(2))/2) % Self::HT_BLOCK_SIZE;
+            let current_address = block_address*Self::HT_BLOCK_SIZE + block_indice;
+            
+            if self.table[block_address][block_indice] == kmer {
+=======
             let current_address = ((hashed_kmer as usize) + (i+i.pow(2))/2) % Self::TABLE_SIZE;
             if self.table[current_address] == kmer {
+>>>>>>> 7ebe2647185b99f07951a3ddf755ec52a03f78bc
                 self.counters[current_address] = self.counters[current_address].saturating_add(1);
                 inserted = true;
             }
